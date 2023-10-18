3a26f936e85c62d91e61685ec678096a6ead8a3f
Mayur Valte <mayurvalte1999@gmail.com>
2023-10-18 19:18:52 +0530

    Initial Commit

---
+++ src/main/java/com/hrmantra/HrmantraApplication.java
@@ -0,0 +1,13 @@
+package com.hrmantra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrmantraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HrmantraApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hrmantra/HrmantraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hrmantra;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrmantraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

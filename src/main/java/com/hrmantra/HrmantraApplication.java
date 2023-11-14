8ab09209dcef1d88cb2a595e78157bc275fd7218
Mayur Valte <mayurvalte1999@gmail.com>
2023-11-14 21:28:16 +0530

    Removed unused imports in Main Class

--- src/main/java/com/hrmantra/HrmantraApplication.java
+++ src/main/java/com/hrmantra/HrmantraApplication.java
@@ -1,12 +1,7 @@
 package com.hrmantra;
 
-import com.hrmantra.userDetails.dao.UserDetailsDao;
-import com.hrmantra.userDetails.model.User;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
 public class HrmantraApplication {
